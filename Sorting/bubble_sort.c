94a025c2a73ed495fc0f40677a3c26cd0a5e76be
AdiPat <adithemagnificent@gmail.com>
2015-07-10 16:53:35 +0530

    Bubble Sort: Stop evaluating elements once list is sorted.
    This provides a small optimization because the outer loop
    doesn't need to run 'n' times where n is the number of elements
    to be sorted.
    
    The list is sorted if,
    In a pass, there are no swaps. In that case the loop terminates.

--- Sorting/bubble_sort.c
+++ Sorting/bubble_sort.c
@@ -6,8 +6,13 @@
 int* bubble_sort(int* vals, int n, bool SortAscending) 
 {
 	int pass,index;
-	for(pass=1; pass<=n; pass++) 
+	pass=0; // Number of passes
+	index=0; // Index of element examined during pass
+	bool isSorted=false; // true if list is sorted
+	int swaps_per_pass=0; // Number of swaps occuring in a pass
+	while(!isSorted)	
 	{
+		swaps_per_pass=0; //reset number of swaps per pass
 		for(index=0; index<(n-1); index++) 
 		{	
 			bool swap=false;
@@ -26,8 +31,16 @@ int* bubble_sort(int* vals, int n, bool SortAscending)
 					int temp = vals[index];
 					vals[index] = vals[index+1];
 					vals[index+1] = temp;
+					swaps_per_pass++;
 			}
+			// We can say that the list is sorted if, 
+			// no passes occur after examining every element. 
+			if(swaps_per_pass == 0 && index == 	n-2) 
+					isSorted=true;
+			// DEBUG: printf("pass: %d , isSorted: %d , swaps_per_pass: %d , index: %d \n", pass,isSorted,swaps_per_pass,index);
 		}
+		++pass;
 	}
+	
 	return vals;
 }
